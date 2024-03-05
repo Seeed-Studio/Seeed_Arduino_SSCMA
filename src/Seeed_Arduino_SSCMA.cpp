9129244f1fed1310e7e1c1ae3eb60f48c3ebe93b
Jaxon <1391548050@qq.com>
2024-03-05 16:01:53 +0800

    fix: fix access fault and modify buf size

--- src/Seeed_Arduino_SSCMA.cpp
+++ src/Seeed_Arduino_SSCMA.cpp
@@ -27,6 +27,8 @@
 
 #include "Seeed_Arduino_SSCMA.h"
 
+#define SPI_CS(x) do { if(_cs >= 0) digitalWrite(_cs, x); } while(0)
+
 SSCMA::SSCMA()
 {
     _wire = &Wire;
@@ -289,17 +291,11 @@ void SSCMA::spi_cmd(uint8_t feature, uint8_t cmd, uint16_t len, uint8_t *data)
         tx_buf[5] = 0xFF;
     }
 
-    if (_cs >= 0)
-    {
-        digitalWrite(_cs, LOW);
-    }
+    SPI_CS(LOW);
     _spi->beginTransaction(SPISettings(_baud, MSBFIRST, SPI_MODE0));
     _spi->transfer(tx_buf, PACKET_SIZE);
     _spi->endTransaction();
-    if (_cs >= 0)
-    {
-        digitalWrite(_cs, HIGH);
-    }
+    SPI_CS(HIGH);
     delay(_wait_delay);
 }
 
@@ -314,58 +310,38 @@ int SSCMA::spi_available()
     }
 
     spi_cmd(FEATURE_TRANSPORT, FEATURE_TRANSPORT_CMD_AVAILABLE, 0, NULL);
-    if (_cs >= 0)
-    {
-        digitalWrite(_cs, LOW);
-    }
+    SPI_CS(LOW);
     _spi->beginTransaction(SPISettings(_baud, MSBFIRST, SPI_MODE0));
     size = _spi->transfer16(0xFFFF);
     _spi->endTransaction();
-    if (_cs >= 0)
-    {
-        digitalWrite(_cs, HIGH);
-    }
+    SPI_CS(HIGH);
     delay(_wait_delay);
     return size;
 }
 
 int SSCMA::spi_read(char *data, int length)
 {
-    uint16_t packets = length / MAX_SPI_PL_LEN;
-    uint16_t remain = length % MAX_SPI_PL_LEN;
-    for (uint16_t i = 0; i < packets; i++)
+    int recv_len = 0;
+    int pl_len = 0;
+    while (recv_len < length)
     {
-        spi_cmd(FEATURE_TRANSPORT, FEATURE_TRANSPORT_CMD_READ, MAX_SPI_PL_LEN, NULL);
-        if (_cs >= 0)
-        {
-            digitalWrite(_cs, LOW);
+        if (_sync >= 0) {
+            if (digitalRead(_sync) == LOW) 
+                return recv_len;
         }
-        _spi->beginTransaction(SPISettings(_baud, MSBFIRST, SPI_MODE0));
-        _spi->transfer(data + i * MAX_SPI_PL_LEN, MAX_SPI_PL_LEN);
-        _spi->endTransaction();
-        if (_cs >= 0)
-        {
-            digitalWrite(_cs, HIGH);
-        }
-    }
-    if (remain)
-    {
-        spi_cmd(FEATURE_TRANSPORT, FEATURE_TRANSPORT_CMD_READ, remain, NULL);
-        if (_cs >= 0)
-        {
-            digitalWrite(_cs, LOW);
-        }
-        _spi->beginTransaction(SPISettings(_baud, MSBFIRST, SPI_MODE0));
-        _spi->transfer(data + packets * MAX_SPI_PL_LEN, remain);
+        pl_len = length - recv_len;
+        pl_len = pl_len > MAX_SPI_PL_LEN ? MAX_SPI_PL_LEN : pl_len;
+        spi_cmd(FEATURE_TRANSPORT, FEATURE_TRANSPORT_CMD_READ, pl_len, NULL);
 
+        SPI_CS(LOW);
+        _spi->beginTransaction(SPISettings(_baud, MSBFIRST, SPI_MODE0));
+        _spi->transfer(data + recv_len, pl_len);
         _spi->endTransaction();
+        SPI_CS(HIGH);
 
-        if (_cs >= 0)
-        {
-            digitalWrite(_cs, HIGH);
-        }
+        recv_len += pl_len;
     }
-    return length;
+    return recv_len;
 }
 
 int SSCMA::spi_write(const char *data, int length)
@@ -490,22 +466,25 @@ int SSCMA::wait(int type, const char *cmd, uint32_t timeout)
     while (millis() - startTime <= timeout)
     {
         int len = available();
-        if (len)
-        {
-            if (len + rx_end > sizeof(rx_buf))
-            {
-                rx_end = 0;
-                return CMD_ENOMEM;
-            }
-            rx_end += read(rx_buf + rx_end, len);
-            rx_buf[rx_end] = '\0';
+        if (len == 0) continue;
+        if (len + rx_end > sizeof(rx_buf)) {
+            rx_end = 0;
         }
-        while (char *suffix = strnstr(rx_buf + rx_ofs, RESPONSE_SUFFIX, rx_end - rx_ofs))
-        {
+
+        rx_end += read(rx_buf + rx_end, len);
+        rx_buf[rx_end] = '\0';
+        rx_ofs = 0;
+
+        while (char *suffix = strnstr(rx_buf + rx_ofs, RESPONSE_SUFFIX, rx_end - rx_ofs)) {
             if (char *prefix = strnstr(rx_buf + rx_ofs, RESPONSE_PREFIX, suffix - rx_buf - rx_ofs))
             {
                 // get json payload
                 len = suffix - prefix;
+                if (len > 4095) { // to long, don't parse
+                    rx_ofs += len + strlen(RESPONSE_SUFFIX);
+                    continue;
+                }
+
                 memcpy(payload, prefix + 1, len);
                 payload[len] = '\0';
                 // Serial.printf("\npayload :%s", payload);
@@ -553,35 +532,41 @@ int SSCMA::wait(int type, const char *cmd, uint32_t timeout)
 void SSCMA::fetch(ResponseCallback RespCallback)
 {
     int len = available();
-    if (len)
-    {
-        if (len + rx_end > sizeof(rx_buf))
-        {
-            rx_end = 0;
-        }
-        rx_end += read(rx_buf + rx_end, len);
-        rx_buf[rx_end] = '\0';
-        Serial.printf("[%d] ->", len);
+    if (len == 0) return;
+    if (len + rx_end >= sizeof(rx_buf)) {
+        rx_end = 0;
     }
+    Serial.printf("\n[%d] ->", len);
+
+    rx_end += read(rx_buf + rx_end, len);
+    rx_buf[rx_end] = '\0';
+    rx_ofs = 0;
+
+    // extract each complete response
     while (char *suffix = strnstr(rx_buf + rx_ofs, RESPONSE_SUFFIX, rx_end - rx_ofs))
     {
-        if (char *prefix = strnstr(rx_buf + rx_ofs, RESPONSE_PREFIX, suffix - rx_buf - rx_ofs))
-        {
+        if (char *prefix = strnstr(rx_buf + rx_ofs, RESPONSE_PREFIX, suffix - (rx_buf + rx_ofs))) {
             len = suffix - prefix + strlen(RESPONSE_SUFFIX);
-            memcpy(payload, prefix, len);
-            payload[len] = '\0';
-            RespCallback(payload);
-        }
-        else
-        {
-            len = suffix - rx_buf + strlen(RESPONSE_SUFFIX);
+            Serial.printf(" %d", len);
+            if (len < sizeof(payload)) {
+                memcpy(payload, prefix, len);
+                payload[len] = '\0';
+                RespCallback(payload);
+            }
+        } else {
+            len = suffix - (rx_buf + rx_ofs) + strlen(RESPONSE_SUFFIX);
             Serial.print(" X");
         }
         rx_ofs += len;
     }
-    memmove(rx_buf, rx_buf + rx_ofs, rx_end - rx_ofs);
-    rx_end -= rx_ofs;
-    rx_ofs = 0;
+    // Serial.printf("\naddr:0x%x, end:%d, ofs:%d\n", rx_buf, rx_end, rx_ofs);
+
+    // move remaining data to the beginning of the buffer
+    if (rx_ofs > 0 && rx_ofs <= rx_end) {
+        memmove(rx_buf, rx_buf + rx_ofs, rx_end - rx_ofs);
+        rx_end -= rx_ofs;
+        rx_ofs = 0;
+    }
 }
 
 int SSCMA::invoke(int times, bool filter, bool show)
