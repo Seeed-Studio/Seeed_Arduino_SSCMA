08e3b913be00a667b1a4ff03ab3990864b740269
nullptr <nullptr@localhost>
2024-04-22 14:31:22 +0800

    chore: update index.html

--- examples/camera_web_server/app_httpd.cpp
+++ examples/camera_web_server/app_httpd.cpp
@@ -848,7 +848,7 @@ static esp_err_t index_handler(httpd_req_t* req) {
     httpd_resp_set_type(req, "text/html");
     httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
 
-    return httpd_resp_send(req, (const char*)index_html_gz, index_html_gz_len);
+    return httpd_resp_send(req, (const char*)web_index_html_gz, web_index_html_gz_len);
 }
 
 void startCameraServer() {
