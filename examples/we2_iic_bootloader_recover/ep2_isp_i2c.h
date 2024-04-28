ae69f51e277b4429d212841e4e05b9e3dfe5c81b
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2024-04-28 15:07:27 +0800

    feat: add we2 i2c bootloader recovery tool example

---
+++ examples/we2_iic_bootloader_recover/ep2_isp_i2c.h
@@ -0,0 +1,18 @@
+/*
+ * ep2_isp_i2c.h
+ *
+ *  Created on: 2022�~10��4��
+ *      Author: 901912
+ */
+
+#ifndef INCLUDE_EP2_ISP_I2C_H_
+#define INCLUDE_EP2_ISP_I2C_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+int ep2_isp_i2c_proc(uint8_t *file_buf, uint32_t file_size);
+
+
+#endif /* INCLUDE_EP2_ISP_I2C_H_ */
