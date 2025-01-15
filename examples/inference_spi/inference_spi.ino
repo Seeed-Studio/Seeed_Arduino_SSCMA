2984ccb2651fc9f6a7043b66b6f5dfffe120e9fd
nullptr <nullptr@localhost>
2025-01-15 08:34:43 +0000

    chore: add spi inference example

---
+++ examples/inference_spi/inference_spi.ino
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Seeed_Arduino_SSCMA.h>
+
+SSCMA AI;
+
+#define SSCMA_CONNECT_TO_XIAO_S3         1
+#define SSCMA_CONNECT_TO_GORVE_VISION_AI 0
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    while (!Serial) delay(1000);
+
+#if SSCMA_CONNECT_TO_XIAO_S3
+    SPI.begin(SCK, MISO, MOSI, -1);
+    AI.begin(&SPI, D3, -1, -1, 400000);
+#endif
+
+#if SSCMA_CONNECT_TO_GORVE_VISION_AI
+    SPI.begin(SCK, MOSI, MISO, -1);
+    AI.begin(&SPI, D1, D0);
+#endif
+
+}
+
+void loop()
+{
+    if (!AI.invoke(1, false, true))
+    {
+        Serial.println("invoke success");
+        Serial.print("perf: prepocess=");
+        Serial.print(AI.perf().prepocess);
+        Serial.print(", inference=");
+        Serial.print(AI.perf().inference);
+        Serial.print(", postpocess=");
+        Serial.println(AI.perf().postprocess);
+
+        for (int i = 0; i < AI.boxes().size(); i++)
+        {
+            Serial.print("Box[");
+            Serial.print(i);
+            Serial.print("] target=");
+            Serial.print(AI.boxes()[i].target);
+            Serial.print(", score=");
+            Serial.print(AI.boxes()[i].score);
+            Serial.print(", x=");
+            Serial.print(AI.boxes()[i].x);
+            Serial.print(", y=");
+            Serial.print(AI.boxes()[i].y);
+            Serial.print(", w=");
+            Serial.print(AI.boxes()[i].w);
+            Serial.print(", h=");
+            Serial.println(AI.boxes()[i].h);
+        }
+        for (int i = 0; i < AI.classes().size(); i++)
+        {
+            Serial.print("Class[");
+            Serial.print(i);
+            Serial.print("] target=");
+            Serial.print(AI.classes()[i].target);
+            Serial.print(", score=");
+            Serial.println(AI.classes()[i].score);
+        }
+        for (int i = 0; i < AI.points().size(); i++)
+        {
+            Serial.print("Point[");
+            Serial.print(i);
+            Serial.print("]: target=");
+            Serial.print(AI.points()[i].target);
+            Serial.print(", score=");
+            Serial.print(AI.points()[i].score);
+            Serial.print(", x=");
+            Serial.print(AI.points()[i].x);
+            Serial.print(", y=");
+            Serial.println(AI.points()[i].y);
+        }
+        for (int i = 0; i < AI.keypoints().size(); i++)
+        {
+            Serial.print("keypoint[");
+            Serial.print(i);
+            Serial.print("] target=");
+            Serial.print(AI.keypoints()[i].box.target);
+            Serial.print(", score=");
+            Serial.print(AI.keypoints()[i].box.score);
+            Serial.print(", box:[x=");
+            Serial.print(AI.keypoints()[i].box.x);
+            Serial.print(", y=");
+            Serial.print(AI.keypoints()[i].box.y);
+            Serial.print(", w=");
+            Serial.print(AI.keypoints()[i].box.w);
+            Serial.print(", h=");
+            Serial.print(AI.keypoints()[i].box.h);
+            Serial.print("], points:[");
+            for (int j = 0; j < AI.keypoints()[i].points.size(); j++)
+            {
+                Serial.print("[");
+                Serial.print(AI.keypoints()[i].points[j].x);
+                Serial.print(",");
+                Serial.print(AI.keypoints()[i].points[j].y);
+                Serial.print("],");
+            }
+            Serial.println("]");
+        }
+        if(!AI.last_image().isEmpty())
+        {
+            Serial.print("Last image:");
+            Serial.println(AI.last_image().c_str());
+        }
+    }
+}
